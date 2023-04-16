cb3210e9aa0b435f8b965164700c8e8a24acdd5c
RobbeVanHoppe <robbevanhoppe@outlook.com>
2023-04-16 19:57:55 +0200

    werkt denk ik

--- backend/springboot/src/main/java/com/darkcandles/springboot/SpringbootApplication.java
+++ backend/springboot/src/main/java/com/darkcandles/springboot/SpringbootApplication.java
@@ -2,12 +2,13 @@ package com.darkcandles.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootApplication.class, args);
+  }
 
 }
